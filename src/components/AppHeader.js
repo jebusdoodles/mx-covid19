aa123b2e831c763d56e4ffafd256a3efc53560f2
ilhuitem0c <jc11111001001@gmail.com>
2020-03-13 15:00:37 -0600

    first commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap'; 
+
+export default class AppHeader extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="light" variant="light">
+                    <Navbar.Brand href="/">MX-COVID19</Navbar.Brand>
+                    <Navbar.Collapse>
+                        <Nav className="justify-content-end" style={{ width: "100%" }}>
+                            <Nav.Link href="/prevencion">Prevención</Nav.Link>
+                            <Nav.Link href="/oficial">Fuentes oficiales</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import AppHeader from './../components/AppHeader';
+
+export default class HomeContainer extends Component {
+    render() {
+        return (
+            <div>
+                <AppHeader></AppHeader>
+            </div>
+        )
+    }
+}
