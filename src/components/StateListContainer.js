9ddb265be4580c5af9cde9971c449afe853f77dc
ilhuitem0c <jc11111001001@gmail.com>
2020-03-17 00:53:05 -0600

    funcion de la tabla de entidades con positivos

--- src/components/StateListContainer.js
+++ src/components/StateListContainer.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Table } from 'react-bootstrap';
+import dataset from '../database/dbestados';
 
-export default class StateListContainer extends Component {
-    render() {
-        return (
-            <div className='TablaConfirmados'>
-                <Table striped>
-                    <thead>
-                        <tr>
-                            <th>Sitio</th>
-                            <th># Confirmados</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>CDMX</td>
-                            <td>12</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
+const StateListContainer = () =>{
+    const handleList = () =>{
+        const datosDiarios = dataset.slice(dataset.length - 32, dataset.length);
+        return(
+            datosDiarios.filter(c => c.confirmados > 0).map( c => 
+                <tr>
+                    <td>{c.nombre}</td>
+                    <td>{c.confirmados}</td>
+                </tr>             
+            )
         )
     }
+    return (
+        <div className='TablaConfirmados'>
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>Sitio</th>
+                        <th># Confirmados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        handleList(dataset)
+                    }
+                </tbody>
+            </Table>
+        </div>
+    )
 }
+
+export default StateListContainer; 
