142e718bd2c5fed87331f85b28fa5b793f6eec97
ilhuitem0c <jc11111001001@gmail.com>
2020-03-18 22:02:08 -0600

    base de datos 18-03

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { FaAngleUp } from 'react-icons/fa';
+import dataset from '../database/dbnacional';
 
-export default class AppHeader extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="gray" variant="light">
-                    <Navbar.Brand href="/">MX COVID-19</Navbar.Brand>
-                    <Navbar.Collapse>
-                        <Nav className="justify-content-end" style={{ width: "100%" }}>
-                            <div className='datosHeader'>
-                                <p>casos: <span><FaAngleUp />+3</span></p>
-                                <p>muertes: <span><FaAngleUp />+3</span></p>
-                            </div>
-                            <Nav.Link href="/prevencion">Prevención</Nav.Link>
-                            <Nav.Link href="/oficial">Fuentes oficiales</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+const AppHeader = () =>{
+    const handleDatos = data =>{
+        let confdiff = data[data.length - 1].confirmados - data[data.length - 2].confirmados;
+        let sosdiff = data[data.length - 1].sospechosos - data[data.length - 2].sospechosos;
+        return(
+            <div className='datosHeader'>
+                <p>confirmados: <span className='color-conf'>+ {confdiff}</span></p>
+                <p>sospechosos: <span className='color-sosdiff'>+ {sosdiff}</span></p>
             </div>
         )
     }
+    return (
+        <div>
+            <Navbar bg="gray" variant="light">
+                <Navbar.Brand href="/">MX COVID-19</Navbar.Brand>
+                <Navbar.Collapse>
+                    <Nav className="justify-content-end" style={{ width: "100%" }}>
+                        {
+                            handleDatos(dataset)
+                        }
+                        <Nav.Link href="/prevencion">Prevención</Nav.Link>
+                        <Nav.Link href="/oficial">Fuentes oficiales</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
+
+export default AppHeader;
