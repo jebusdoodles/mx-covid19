39183e9781144cdb91578f0b8d80c86aa0c1892d
ilhuitem0c <jc11111001001@gmail.com>
2020-03-23 21:03:35 -0600

    base de datos actualizada

--- extractinfo/extract_info.py
+++ extractinfo/extract_info.py
@@ -2,7 +2,7 @@
 #más información en https://github.com/chezou/tabula-py
 import tabula
 #insertamos el pdf de casos confirmados
-df = tabula.read_pdf("confirmados.pdf", pages='all')
+df = tabula.read_pdf("confirmados.pdf", lattice=True, pages='all')
 
 #extraemos la información en un archivo .json .csv
 tabula.convert_into("confirmados.pdf", "output.csv", output_format="csv", pages='all')
