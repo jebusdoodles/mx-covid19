b0abfcffd142117253d2b8387c12f5626c152f6d
ilhuitem0c <jc11111001001@gmail.com>
2020-04-20 21:52:06 -0500

    bd 20 04 2020

--- extractinfo/more_info.py
+++ extractinfo/more_info.py
@@ -4,10 +4,13 @@ import pandas as pd
 df = pd.read_csv("covidata.csv", encoding = "ISO-8859-1")
 
 file_name = 'tlaxcala.csv'
-
 #Traeme de esta entidad lo que como resultado den positivo
-df = df[(df['ENTIDAD_UM'] == 29) & (df['RESULTADO'] == 1)]
 
-df.to_csv(file_name, sep=',', encoding='utf-8')
+df = df[(df['ENTIDAD_UM'] == 7) & (df['RESULTADO'] == 1)]
+#df = df[(df['RESULTADO'] == 3)]
+#df = df[(df['FECHA_DEF'] != '9999-99-99')]
+
+#df.to_csv(file_name, sep=',', encoding='utf-8')
+
 #lenght 
 print(df.shape[0])
