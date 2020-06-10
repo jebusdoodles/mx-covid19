d86e671917a1d0a327017b3c836749f026670096
ilhuitem0c <jc11111001001@gmail.com>
2020-06-09 20:45:43 -0500

    db 09 06 2020 y small prospecto de mapa

--- src/components/DailySumTable.js
+++ src/components/DailySumTable.js
@@ -6,8 +6,25 @@ const DailySumTable = () => {
         let datosDiarios = alldata.slice(alldata.length - 32, alldata.length);
         let datosPasados = alldata.slice(alldata.length - 64, alldata.length - 32);
         let otherData = [];
+        let ultimo = 32; 
+        let final =64;
+
         let dataListado = () => {
-            let datosTabla = [];  
+            let datosTabla = [];  /*
+            for(var j=0; j < 4; j++){
+                for(var i = 0; i < datosDiarios.length; i++){
+                    datosTabla.push({
+                        nombre: i,
+                        diferencia: datosDiarios[i].confirmados - datosPasados[i].confirmados,
+                    });
+                }
+                datosDiarios = datosPasados
+
+                ultimo = ultimo + 32 
+                final = final + 32 
+                
+                datosPasados = alldata.slice(alldata.length - final, alldata.length - ultimo)
+            }*/
             for(var i = 0; i < datosDiarios.length; i++){
                 datosTabla.push({
                     nombre: datosDiarios[i].nombre,
