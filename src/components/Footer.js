74b701a0f268389f3ba2fb012c374654d2cf1477
ilhuitem0c <jc11111001001@gmail.com>
2021-02-08 20:55:29 -0600

    2021 02 8

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,7 @@ const Footer = () =>{
                 </Row>
                 <Row className="justify-content-md-center">
                     <div className='footer-container'>
-                        <p className='footer-text'>Hecho con <FaHeart /> por <a href='https://twitter.com/JebusDoodles'>Jesús Cortés</a>. Quédense en casa.</p>
+                        <p className='footer-text'>Hecho con <FaHeart /> por <a href='https://jebusdoodles.github.io/'>Jesús Cortés</a>. Quédense en casa.</p>
                     </div>
                 </Row>
             </Container>
