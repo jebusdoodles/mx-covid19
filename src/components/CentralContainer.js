37ef42acfb2e97239c755e6934e79f65052401f5
ilhuitem0c <jc11111001001@gmail.com>
2021-07-07 21:18:54 -0500

    2021 07 7

--- src/components/CentralContainer.js
+++ src/components/CentralContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Alert } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import StateMapContainer from './StateMapContainer';
 import StateListContainer from './StateListContainer'; 
 import StateDataContainer from './StateDataContainer';
