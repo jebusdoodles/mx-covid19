5377597ea15aef25e18f9bad9257ed38124d43e2
ilhuitem0c <jc11111001001@gmail.com>
2022-05-30 19:10:42 -0500

    2022 05 30

--- src/components/LastUpdate.js
+++ src/components/LastUpdate.js
@@ -7,7 +7,7 @@ const LastUpdate = () =>{
     //let date_update = dataset[dataset.length - 1].fechaformat;
     return (
         <Alert variant='danger' className='alerta-estilos'>
-            <FaRegClock /> Sitio cerrado el 26 de abril del 2016.  <Alert.Link href="https://twitter.com/SSalud_mx/status/1519116887149404160">SSA</Alert.Link>
+            <FaRegClock /> Sitio cerrado el 26 de abril del 2022.  <Alert.Link href="https://twitter.com/SSalud_mx/status/1519116887149404160">SSA</Alert.Link>
         </Alert>
     );
 }
